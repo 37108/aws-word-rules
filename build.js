6b2abe8e4ee419d158ad482921a41df7d22f8c81
37108 <37108mail@gmail.com>
2019-07-03 19:13:19 +0900

    [fix] add flag

--- build.js
+++ build.js
@@ -6,9 +6,12 @@ const build = () => {
   const file = path.resolve(__dirname, 'phr.yml')
   const config = prh.fromYAMLFilePath(file)
   const results = config.rules.map(rule => {
+    const flag = rule.pattern.flags || rule.pattern.toString().match(/[gimuy]*$/)[0]
+
     const result = {
       pattern: rule.pattern.source,
       expected: rule.expected,
+      flag: rule.pattern.flags !== null ? rule.pattern.flags : rule.pattern.toString().match(/[gimuy]*$/)[0]
     }
     return result
   })
