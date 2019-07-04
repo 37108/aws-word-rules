df8bbc0e40dccb24b7f8ebec585abaf3faa26f61
37108 <37108mail@gmail.com>
2019-07-04 14:55:38 +0900

    Hotfix 3 (#6)
    * [fix] remove flag
    
    正規表現用のflagを一旦削除
    
    * 1.3.0

--- build.js
+++ build.js
@@ -6,14 +6,10 @@ const build = () => {
   const file = path.resolve(__dirname, 'prh.yml')
   const config = prh.fromYAMLFilePath(file)
   const results = config.rules.map(rule => {
-    const flag = rule.pattern.flags || rule.pattern.toString().match(/[gimuy]*$/)[0]
-
-    const result = {
+    return {
       pattern: rule.pattern.source,
       expected: rule.expected,
-      flag: rule.pattern.flags !== null ? rule.pattern.flags : rule.pattern.toString().match(/[gimuy]*$/)[0]
     }
-    return result
   })
   fs.writeFile('all.json', JSON.stringify(results), err => {
     console.log(err)
